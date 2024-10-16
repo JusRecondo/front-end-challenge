317bf1d4ecc255b40d997160d9ddca748b625116
JusRecondo <justina.recondo@gmail.com>
2024-10-15 22:21:37 -0300

    feat: Add map section

--- src/components/mapSection/MapSection.jsx
+++ src/components/mapSection/MapSection.jsx
@@ -1,6 +1,21 @@
+import { useContext } from "react"
+import { DashboardContext } from "../../context/DashboardContext"
+
 const MapSection = () => {
+  const { fieldData } = useContext(DashboardContext)
   return (
-    <div>MapSection</div>
+    <section className="map-section">
+      <h2>Ubicación: {fieldData.location.name}</h2>
+      <iframe 
+        className="location-map" 
+        src={fieldData.location.map} 
+        allowFullScreen="" 
+        loading="lazy" 
+        referrerPolicy="no-referrer-when-downgrade"
+        title="Ubicación del campo"
+      >
+      </iframe>
+    </section>
   )
 }
 
